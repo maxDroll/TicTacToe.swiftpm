346588b492088706cf8023cf12087834ca56b0f3
georgie naegele <gnaegele5254@stu.d214.org>
2024-04-19 12:55:32 -0500

    I turned the rounded rectangle and text into a navigation link to the easy view, then made ones for the medium and hard views

--- difficultySelect.swift
+++ difficultySelect.swift
@@ -14,24 +14,56 @@ struct difficultySelect: View {
     var body: some View {
         VStack {
             Text("Choose Your Difficulty!")
-            HStack{
-                
-                RoundedRectangle(cornerSize: CGSize(width: 50, height: 50))
+                .font(.system(size: 100))
+            NavigationStack{
+
+            
+                HStack{
+                    NavigationLink {
+                        EasyView()
+                    } label: {
+                        RoundedRectangle(cornerSize: CGSize(width: 50, height: 50))
                             .frame(width:400, height: 200)
                             .foregroundColor(.green)
                             .overlay(
-                                Text("Easy")
-                                    .foregroundColor(.black)
+                                Text("Easy")                                .foregroundColor(.black)
                                     .font(.system(size: 75))
                             )
                             .padding()
-                  
-               
-                    
-                
-                
-                
                     }
+
+                    NavigationLink {
+                        MediumView()
+                    } label: {
+                        RoundedRectangle(cornerSize: CGSize(width: 50, height: 50))
+                            .frame(width:400, height: 200)
+                            .foregroundColor(.yellow)
+                            .overlay(
+                                Text("Medium")                                .foregroundColor(.black)
+                                    .font(.system(size: 75))
+                            )
+                            .padding()
+                    }
+                    
+                    
+                    
+                    NavigationLink {
+                        HardView()
+                    } label: {
+                        RoundedRectangle(cornerSize: CGSize(width: 50, height: 50))
+                            .frame(width:400, height: 200)
+                            .foregroundColor(.red)
+                            .overlay(
+                                Text("Hard")                                .foregroundColor(.black)
+                                    .font(.system(size: 75))
+                            )
+                            .padding()
+                    }
+                    
+                   
+                    
+                }
+            }
         }
     }
 }
