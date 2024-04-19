19a8a29bdbefb236b8120be5e225a16abf15b926
maxDroll <mdroll6088.stu.d214.org>
2024-04-19 12:55:42 -0500

    Added a brain

--- MyApp.swift
+++ MyApp.swift
@@ -2,9 +2,11 @@ import SwiftUI
 
 @main
 struct MyApp: App {
+    @StateObject var BigBoi = Everything()
     var body: some Scene {
         WindowGroup {
             difficultySelect()
+                .environmentObject(BigBoi)
         }
     }
 }
