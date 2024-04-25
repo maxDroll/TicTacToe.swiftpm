0b67ce23f2932d3b4bfcc33643c85885a09670c9
Michael Peroulas <mperoulas4342@stu.d214.org>
2024-04-25 13:12:29 -0500

    Created the Text tracker for the wins, losses, and ties. Connected it to the fucntion so it shows the actual wins, losses, and ties. Also brought the Tic Tac Toe board into the easy view

--- EasyView.swift
+++ EasyView.swift
@@ -42,21 +42,34 @@ struct EasyView: View {
                        
                     }
             }
+            ZStack{
+                
+                VStack{
+                    Text("Wins: \(everything.wins)")
+                    Text("Loses: \(everything.losses)")
+                    Text("Ties: \(everything.ties)")
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title)
+//                HStack(spacing: 170){
+//                    RoundedRectangle(cornerRadius: 10)
+//                        .frame(width: 10, height: 500)
+//                    RoundedRectangle(cornerRadius: 10)
+//                        .frame(width: 10, height: 500)
+//                }
+//                VStack(spacing: 170){
+//                    RoundedRectangle(cornerRadius: 10)
+//                        .frame(width: 500, height: 10)
+//                    RoundedRectangle(cornerRadius: 10)
+//                        .frame(width: 500, height: 10)
+//                }
+                
+            }
+            
+            
+            
                 }
             }
         }
     
 
-
-//            RoundedRectangle(cornerRadius: 10)
-//                       .frame(width: 10, height: 500)
-//                       .offset(x: -100, y: 306)
-//            RoundedRectangle(cornerRadius: 10)
-//                       .frame(width: 10, height: 500)
-//                       .offset(x: 100, y: -200)
-//            RoundedRectangle(cornerRadius: 10)
-//                       .frame(width: 500, height: 10)
-//                       .offset(x: 0, y: -400)
-//            RoundedRectangle(cornerRadius: 10)
-//                       .frame(width: 500, height: 10)
-//                       .offset(x: 0, y: -600)
